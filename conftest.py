a96e9f09a42e2f3143933e857cb94aa765611cb9
Frédéric Collonval <fcollonval@users.noreply.github.com>
2024-12-03 12:39:43 +0100

    First working version

--- conftest.py
+++ conftest.py
@@ -1,7 +1,12 @@
 import pytest
 
-pytest_plugins = ["jupyter_server.pytest_plugin"]
+pytest_plugins = ["jupyter_server.pytest_plugin", "jupyter_server_ydoc.pytest_plugin"]
+
 
 @pytest.fixture
 def jp_server_config(jp_server_config):
-     return {"ServerApp": {"jpserver_extensions": {"jupyter_nbmodel_client": True}}}
+    return {
+        "ServerApp": {
+            "jpserver_extensions": {"jupyter_server_ydoc": True, "jupyter_server_fileid": True}
+        }
+    }
--- jupyter_nbmodel_client/__init__.py
+++ jupyter_nbmodel_client/__init__.py
@@ -1,3 +1,7 @@
 """Client to interact with Jupyter notebook model."""
 
+from .client import NbModelClient
+
 __version__ = "0.1.0"
+
+__all__ = ["NbModelClient"]
