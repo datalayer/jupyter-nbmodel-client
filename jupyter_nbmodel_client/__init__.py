95dee687e8b2a7773df9bfcd8fd58a8790eab8a2
Frédéric Collonval <fcollonval@users.noreply.github.com>
2025-02-20 14:58:30 +0100

    Move ws helpers to specific file

--- jupyter_nbmodel_client/__init__.py
+++ jupyter_nbmodel_client/__init__.py
@@ -7,7 +7,8 @@
 from nbformat import NotebookNode
 
 from .agent import AIMessageType, BaseNbAgent
-from .client import NbModelClient, get_datalayer_websocket_url, get_jupyter_notebook_websocket_url
+from .client import NbModelClient
+from .helpers import get_datalayer_websocket_url, get_jupyter_notebook_websocket_url
 from .model import KernelClient, NotebookModel
 
 __version__ = "0.8.0"
